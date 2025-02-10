87a7b0dea11b3e4c9ec34b9904d20407957337a9
Devran Perdana Malik <101850311+dvpms@users.noreply.github.com>
2025-02-10 16:52:42 +0700

    Revert "Feature/home"

--- src/components/layout/Footer/components/LanguageSelector.js
+++
@@ -1,9 +0,0 @@
-export default function LanguageSelector() {
-    return (
-      <div className="flex justify-center gap-4 text-sm text-white mb-4">
-        <button className="hover:text-gray-100 transition-colors">Indonesia</button>
-        <span>|</span>
-        <button className="hover:text-gray-100 transition-colors">English</button>
-      </div>
-    )
-  }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,51 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './src/pages/**/*.{js,jsx,mdx}',
-    './src/components/**/*.{js,jsx,mdx}',
-    './src/app/**/*.{js,jsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        // Palet Tosca
-        tosca: {
-          50: '#7BFCF3',   // Paling Terang
-          100: '#4BD3CB',
-          200: '#3AA9A2',  // Primary
-          300: '#2A817B',
-          400: '#1B5857',
-          500: '#0D3735',  // Paling Gelap
-        },
-        // Palet Biru
-        blue: {
-          50: '#DAE8FA',   // Paling Terang
-          100: '#95C3F2',
-          200: '#4E9EDD',  // Primary
-          300: '#3A78A9',
-          400: '#275478',
-          500: '#051420',  // Paling Gelap
-        },
-        // Palet Hijau
-        green: {
-          50: '#85FBB3',   // Paling Terang
-          100: '#4BD487',
-          200: '#3AA96B',  // Primary
-          300: '#2A8050',
-          400: '#1B5936',
-          500: '#031409',  // Paling Gelap
-        },
-        // Palet Abu-abu
-        gray: {
-          50: '#E3E9E9',   // Paling Terang
-          100: '#BCC1C1',
-          200: '#969A9A',  // Primary
-          300: '#727575',
-          400: '#505252',
-          500: '#131414',  // Paling Gelap
-        }
-      }
-    }
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
-}
+};
