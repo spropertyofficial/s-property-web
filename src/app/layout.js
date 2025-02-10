1812bbca5830aa5b6d978f0337fa2dc61872b775
Devran <devranmalik82@gmail.com>
2025-02-10 16:48:23 +0700

    feat: create footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
-import { Inter } from 'next/font/google'
-import '@/styles/globals.css'
-import Footer from '@/components/layout/Footer'
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'S-Property',
-  description: 'Find your dream property',
-}
+  title: "S-Property",
+  description: "Find your dream property",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        <Header />
+        {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import CategoryMenu from '@/components/CategoryMenu'
-import Header from '@/components/Header'
-import PropertyListing from '@/components/property/PropertyListing'
+import CategoryMenu from "@/components/sections/Home/CategoryMenu";
+import ExploreCities from "@/components/sections/Home/ExploreCities";
+import PropertyListing from "@/components/sections/Home/PropertyListing";
+import SearchSection from "@/components/sections/Home/SearchSection";
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <CategoryMenu />
-      <PropertyListing />
-
+      <SearchSection/>
+      <CategoryMenu/>
+      <ExploreCities/>
+      <PropertyListing/>
     </main>
   )
 }
