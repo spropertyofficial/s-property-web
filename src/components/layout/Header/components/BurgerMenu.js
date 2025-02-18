5e23a21b6a022639cb7a89583acde14c89b99bb7
Devran <devranmalik82@gmail.com>
2025-02-18 10:49:27 +0700

    refactor: Returns the correct menu sections

--- src/components/layout/Header/components/BurgerMenu.js
+++ src/components/layout/Header/components/BurgerMenu.js
@@ -1,9 +1,9 @@
-// src/components/layout/Header/components/BurgerMenu.js
 "use client"
 
 import { useState } from 'react'
 import Link from 'next/link'
 import { Menu, X } from 'lucide-react'
+import Image from 'next/image'
 
 export default function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false)
@@ -19,36 +19,28 @@ export default function BurgerMenu() {
 
   return (
     <div>
-      {/* Burger Icon - Menggunakan div bukan button */}
-      <div 
+      {/* Burger Icon */}
+      <button 
         onClick={() => setIsOpen(!isOpen)}
-        className="text-tosca-200 hover:text-tosca-300 transition-colors cursor-pointer"
+        className="text-tosca-200 hover:text-tosca-300 transition-colors"
       >
         {isOpen ? <X size={30} /> : <Menu size={30} />}
-      </div>
+      </button>
 
-      {/* Overlay */}
+      {/* Menu Overlay */}
       {isOpen && (
-        <div 
-          className="fixed inset-0 bg-black/50 z-40" 
-          onClick={() => setIsOpen(false)} 
-        />
+        <div className="fixed inset-0 top-20 bg-black/50 z-40" onClick={() => setIsOpen(false)} />
       )}
 
       {/* Menu Content */}
       <div className={`
-        fixed top-0 left-0 h-full w-[280px] bg-white z-50 
-        transform transition-transform duration-300
+        fixed top-20 left-0 h-full w-[280px] bg-tosca-100 z-50 transform transition-transform duration-300
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
       `}>
         <div className="p-4">
-          {/* Logo */}
-          <div className="mb-8">
-            <img src="/images/logo.png" alt="S-Property" className="h-8" />
-          </div>
 
           {/* Menu Items */}
-          <nav className="space-y-4">
+          <nav className="space-y-3 text-left">
             {menuItems.map((item, index) => (
               <Link 
                 key={index}
