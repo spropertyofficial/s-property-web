2fb487c2ce81bc654c781955bc105a5f8251db6f
Devran <devranmalik82@gmail.com>
2025-02-25 10:31:02 +0700

    feat: Whatsapp Button for user

--- src/components/sections/Property/Header/Header.js
+++ src/components/sections/Property/Header/Header.js
@@ -1,3 +1,4 @@
+import { formatToShortRupiah } from '@/utils/formatCurrency'
 import ActionButtons from './components/ActionButtons'
 import PropertyLabels from './components/PropertyLabels'
 
@@ -10,7 +11,11 @@ export default function Header({ title, price, developer, transaction, type}) {
       </div>
       {type && <h2 className="text-xs text-gray-300">{type}</h2>}
       <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
-      <div className="text-base text-gray-900">Start from <span className='font-semibold text-xl text-tosca-500'>Rp. {price}</span></div>
+          <div className="text-base text-gray-900">
+            Start from <span className='font-semibold text-xl text-tosca-500'>
+              {formatToShortRupiah(price)}
+            </span>
+          </div>
     </div>
   )
 }
