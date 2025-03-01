9e8baa621cadd339d1a5e73e97d562b6ca414a87
Devran <devranmalik82@gmail.com>
2025-03-01 17:02:53 +0700

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.pexels.com', 'picsum.photos'],
-    },
-  }
+  reactStrictMode: true,
+
+  images: {
+    domains: ["images.pexels.com", "picsum.photos"],
+  },
+
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+
+  transpilePackages: ["sweetalert2", "notyf", "react-datepicker"],
+};
 
 export default nextConfig;
